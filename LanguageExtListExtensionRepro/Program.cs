ef6dec197dd85d7856f78c72c338bb54869c2248
Atte Jantunen <atte_jantunen@trimble.com>
2023-11-24 14:32:17 +0200

    Initial commit

---
+++ LanguageExtListExtensionRepro/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+var foo = new List<Foo>();
+var bar = new List<Foo>().Zip(foo).All(c => c.First == c.Second);
+var barz = foo.Append(new() { Barz = new Bar[] { } });
+
+internal record Foo
+{
+    public Bar[] Barz { get; set; }
+}
+
+internal record Bar { }
